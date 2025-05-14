5d7b6e8930e3fdf41c89f74ef63e4d57102d7dca
kp-of-s <gimbhs@gmail.com>
2025-05-14 14:44:31 +0900

    initial commit

---
+++ src/main/java/com/example/TalkToDo/TalkToDoApplication.java
@@ -0,0 +1,13 @@
+package com.example.TalkToDo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalkToDoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TalkToDo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TalkToDo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
