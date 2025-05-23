13a9759e1099457ae2d3047d0e849805bb56e422
wns0901 <wnswns012@gmail.com>
2025-05-23 15:11:05 +0900

    user 더미 생성

--- src/test/java/com/example/TalkToDo/test.java
+++ src/test/java/com/example/TalkToDo/test.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.example.TalkToDo.repository.UserRepository;
 
-
 @SpringBootTest
 public class test {
 
@@ -32,5 +31,24 @@ public class test {
     User user2 = userRepository.findById(user.getId()).orElseThrow(() -> new RuntimeException("User not found"));
     System.out.println(user2.getName());
     System.out.println("테스트 성공");
+  }
+
+  @Test
+  public void CreateDummyUser() {
+    String[] names = { "김민준", "이서연", "박지후", "최예린", "정하늘", "오지민", "한도윤", "유하람", "신유진", "장서준" };
+
+    String[] department = { "개발부", "인사팀", "영업팀", "마케팅팀", "경영팀" };
+    String[] position = { "사원", "대리", "과장", "부장", "임원" };
+
+    for (int i = 0; i < 10; i++) {
+      User user = new User();
+      user.setName(names[i]);
+      user.setEmail("user" + i + "@test.com");
+      user.setDepartment(department[i % department.length]);
+      user.setPosition(position[i % position.length]);
+      user.setUsername("user" + i);
+      user.setPassword(passwordEncoder.encode("1234"));
+      userRepository.save(user);
+    }
   }
 }
