6cc78fdd81276099f2e00afe34975e4ada76b91c
wns0901 <wnswns012@gmail.com>
2025-05-23 12:23:40 +0900

    파일구조 변경

--- src/main/java/com/example/TalkToDo/TalkToDoApplication.java
+++ src/main/java/com/example/TalkToDo/TalkToDoApplication.java
@@ -2,14 +2,8 @@ package com.example.TalkToDo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication(scanBasePackages = "com.talktodo")
-@EnableJpaRepositories(basePackages = "com.talktodo.repository")
-@EntityScan(basePackages = "com.talktodo.entity")
-@EnableJpaAuditing
+@SpringBootApplication
 public class TalkToDoApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/example/TalkToDo/DemoApplicationTests.java
+++ src/test/java/com/example/TalkToDo/DemoApplicationTests.java
@@ -2,12 +2,34 @@ package com.example.TalkToDo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.TalkToDo.repository.UserRepository;
+import com.example.TalkToDo.entity.User;
 
 @SpringBootTest
 class TalkToDoApplicationTests {
 
+	@Autowired
+	private UserRepository userRepository;
+
 	@Test
 	void contextLoads() {
+		
+	}
+
+	@Test
+	void test2() {
+		User user = new User();
+		user.setName("test");
+		user.setEmail("test@test.com");
+		user.setDepartment("test");
+		user.setPosition("test");
+		user.setUsername("test");
+		user.setRole("test");
+		userRepository.save(user);
+
+		User user2 = userRepository.findById(user.getId()).orElseThrow(() -> new RuntimeException("User not found"));
+		System.out.println(user2.getName());
 	}
 
 }
